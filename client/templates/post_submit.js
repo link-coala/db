5ef2eb801b5a360f82cb38a2f1f08f0a23426e5b
link-coala <mpj183@aol.com>
2015-03-23 17:53:34 -0500

    76

--- client/templates/post_submit.js
+++ client/templates/post_submit.js
@@ -7,7 +7,12 @@ Template.postSubmit.events({
       title: $(e.target).find('[name=title]').val()
     };
 
-    post._id = Posts.insert(post);
-    Router.go('postPage', post);
+    // display the error to the user and abort
+    Meteor.call('postInsert', post, function(error, result) {
+    if (error)
+	return alert(error.reason);
+	
+	Router.go('postPage', {_id: result._id});
+	});
   }
 });
